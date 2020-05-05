cbd9a5411696df91dbb4b1a8a71e1e9fca80a503
SrishtiSk <srishtisk.is16@sahyadri.edu.in>
2020-05-05 17:23:55 +0530

    1 edit

---
+++ week 1- Let it Rock/Asteroids/Assets/scripts/Asteroids.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 
+/// </summary>
+/// 
+
+public class Asteroids : MonoBehaviour
+{
+    [SerializeField] Sprite[] asteroids = new Sprite[3];
+    
+    
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = asteroids[Random.Range(0, asteroids.Length)];
+
+        // apply impulse force to get game object moving
+        const float MinImpulseForce = 3f;
+        const float MaxImpulseForce = 5f;
+        float angle = Random.Range(0, 2 * Mathf.PI);
+        Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+        float magnitude = Random.Range(MinImpulseForce, MaxImpulseForce);
+        GetComponent<Rigidbody2D>().AddForce(direction * magnitude, ForceMode2D.Impulse);
+    }
+}
---
+++ week 1- Let it Rock/Asteroids/Assets/scripts/ScreenWrapper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenWrapper : MonoBehaviour
+{
+    
+    float colliderRadius;
+
+
+    void Start()
+    {
+        // saved for efficiency
+       
+        colliderRadius = GetComponent<CircleCollider2D>().radius;
+    }
+    void OnBecameInvisible()
+    {
+        Vector2 position = transform.position;
+
+        // check left, right, top, and bottom sides
+        if (position.x + colliderRadius < ScreenUtils.ScreenLeft ||
+            position.x - colliderRadius > ScreenUtils.ScreenRight)
+        {
+            position.x *= -1;
+        }
+        if (position.y - colliderRadius > ScreenUtils.ScreenTop ||
+            position.y + colliderRadius < ScreenUtils.ScreenBottom)
+        {
+            position.y *= -1;
+        }
+
+        // move ship
+        transform.position = position;
+    }
+
+}
